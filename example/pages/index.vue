5269b1a24e897fc9d269867033440f11cde5cd33
Patrick Cate <patrickcate@users.noreply.github.com>
2020-12-12 16:36:19 -0500

    chore: add nuxt-jsonapi logo and update example to show it

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>Nuxt JSON:API Module</h1>
+  <div class="container">
+    <h1 class="text-center">Nuxt JSON:API Module</h1>
+    <img
+      src="/nuxt-jsonapi-logo.svg"
+      alt="Nuxt JSON:API logo"
+      width="400"
+      height="104"
+      class="logo center"
+    />
     <ul v-if="articles.length">
       <li v-for="article in articles" :key="article.id">
         {{ article.title }}
@@ -23,3 +30,27 @@ export default {
   }
 }
 </script>
+
+<style>
+.logo {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+.container {
+  font-family: sans-serif;
+  max-width: 600px;
+  padding: 1rem;
+  margin: 0 auto;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
